716c0e0beac1a3a8056c6be88748d26f0c78e551
Gustavo Damasceno Algebaile <gustavo.algebaile@al.infnet.edu.br>
2023-11-27 23:12:15 -0300

    Adicionar arquivos de projeto.

---
+++ TP3/Models/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using TP3.Models;
+
+namespace TP3.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+        }
+
+        public DbSet<Produto> Produto { get; set; }
+        public DbSet<TP3.Models.Livro> Livro { get; set; } = default!;
+        public DbSet<TP3.Models.Time> Time { get; set; } = default!;
+
+    }
+}
